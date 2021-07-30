7e84ce8c9336d4a3db212623fa45107e502a22cc
Katherine23-z <eszaharova23@yandex.ru>
2021-07-30 20:48:00 +0300

    init commit

---
+++ app/src/main/java/com/example/mynasaapplication/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.mynasaapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.mynasaapplication.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                    .replace(R.id.container, MainFragment.newInstance())
+                    .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mynasaapplication/ui/Navigation.kt
@@ -0,0 +1,4 @@
+package com.example.mynasaapplication.ui
+
+class Navigation {
+}
